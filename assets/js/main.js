5e6c880a9c99074ec17ed2eebdea0a16e3f2ed25
Bourbon-Rye <69958552+Bourbon-Rye@users.noreply.github.com>
2024-04-17 17:33:07 +0800

    Updated JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,11 +24,79 @@
 				$body.removeClass('is-preload');
 			}, 100);
 		});
+		
+	// Nav.
+		var $nav_a = $nav.find('a');
+
+		$nav_a
+			.addClass('scrolly')
+			.on('click', function(e) {
+
+				var $this = $(this);
+
+				// External link? Bail.
+					if ($this.attr('href').charAt(0) != '#')
+						return;
+
+				// Prevent default.
+					e.preventDefault();
+
+				// Deactivate all links.
+					$nav_a.removeClass('active');
+
+				// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+					$this
+						.addClass('active')
+						.addClass('active-locked');
+
+			})
+			.each(function() {
+
+				var	$this = $(this),
+					id = $this.attr('href'),
+					$article = $(id);
+
+				// No section for this link? Bail.
+					if ($article.length < 1)
+						return;
+
+				// Scrollex.
+					$article.scrollex({
+						mode: 'middle',
+						top: '-10vh',
+						bottom: '-10vh',
+						initialize: function() {
+
+							// Deactivate section.
+								$article.addClass('inactive');
+
+						},
+						enter: function() {
+
+							// Activate section.
+								$article.removeClass('inactive');
+
+							// No locked links? Deactivate all links and activate this section's one.
+								if ($nav_a.filter('.active-locked').length == 0) {
+
+									$nav_a.removeClass('active');
+									$this.addClass('active');
+
+								}
+
+							// Otherwise, if this section's link is the one that's locked, unlock it.
+								else if ($this.hasClass('active-locked'))
+									$this.removeClass('active-locked');
+
+						}
+					});
+
+			});
 
 	// Scrolly.
 		$('#nav a, .scrolly').scrolly({
-			speed: 1000,
-			offset: function() { return $nav.height(); }
+			speed: 1500,
+			/*offset: function() { return $nav.height(); }*/
 		});
-
+		
 })(jQuery);
