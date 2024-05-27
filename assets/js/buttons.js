d4d521fde033a6fe8edb913e9e26a0dc0329ac50
Bourbon-Rye <rydiaz@up.edu.ph>
2024-05-27 22:48:47 +0800

    PUSHHHHH (with working nav)

--- assets/js/buttons.js
+++ assets/js/buttons.js
@@ -3,11 +3,20 @@ var modals = document.querySelectorAll('.modal');
 var X = document.getElementsByClassName("button-round close");
 var exit = document.getElementsByClassName("close-text");
 var menu = document.getElementById("menu");
+var nav = document.getElementsByClassName('navmen');
 
 
 // When the user clicks the button, open the modal
 for (var i = 0; i < btn.length; i++) {
     btn[i].onclick = function(e) {
+        e.preventDefault();
+        modal = document.querySelector(e.target.getAttribute("href"));
+        modal.style.display = "block";
+    }
+}
+
+for (var i = 0; i < nav.length; i++) {
+    nav[i].onclick = function(e) {
         e.preventDefault();
         modal = document.querySelector(e.target.getAttribute("href"));
         modal.style.display = "block";
@@ -23,7 +32,7 @@ for (var i = 0; i < X.length; i++) {
     }
 }
 
-for (var i = 0; i < X.length; i++) {
+for (var i = 0; i < exit.length; i++) {
     exit[i].onclick = function() {
         for (var index in modals) {
             if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";
